d2719cb991d0f08587f8498634e6e6219453a156
huzhengen <huzhengen@163.com>
2022-02-21 15:50:54 +0800

    是否登陆

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
   end
 
   def me
-    render_resource current_user
+    # render_resource current_user
+    return head 404 if current_user.nil?
+    if current_user.errors.empty?
+      render json: { resource: current_user, isLogin: true }, status: 200
+    else
+      err = current_user.errors.messages[:email][0] || current_user.errors.messages[:password][0] || current_user.errors.messages[:password_confirmation][0]
+      render json: { error: err }, status: 422
+    end
   end
 
   def create_params
